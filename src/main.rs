b916a64d60ae0c87e7cebd82b30b8c12774247f1
David Laban <alsuren@gmail.com>
2020-02-25 17:40:33 +0000

    add anyhow context to errors

--- src/main.rs
+++ src/main.rs
@@ -10,37 +10,38 @@ use std::{
     process::{self, Command},
     str,
 };
+use anyhow::{Context};
 
-fn main() -> io::Result<()> {
+fn main() -> anyhow::Result<()> {
     let cargo = env::var("CARGO").unwrap_or_else(|_| "cargo".to_string());
     let exitcode = loop {
         eprint!("Running tests...");
         let output = Command::new(&cargo)
             .arg("test")
             .args(env::args_os().skip(1).skip_while(|s| s == "fixeq"))
-            .output()?;
+            .output().context("running tests")?;
 
-        let forward_output = || -> io::Result<()> {
+        let forward_output = || -> anyhow::Result<()> {
             eprintln!("Last 'cargo test' output:");
-            io::stderr().flush()?;
-            io::stdout().write_all(&output.stdout)?;
-            io::stderr().write_all(&output.stderr)?;
+            io::stderr().flush().context("flushing stderr")?;
+            io::stdout().write_all(&output.stdout).context("forwarding test stdout")?;
+            io::stderr().write_all(&output.stderr).context("forwarding test stderr")?;
             Ok(())
         };
 
         if output.status.success() {
             eprintln!(" succeeded.");
-            forward_output()?;
+            forward_output().context("reporting success")?;
             break 0;
         }
 
         let out = str::from_utf8(&output.stdout).unwrap_or("");
         let failures = parse_out::find_assert_eq_failures(out);
-        let count = fix::fix(failures)?;
+        let count = fix::fix(failures).context("fixing failures")?;
 
         if count == 0 {
             eprintln!(" failed.");
-            forward_output()?;
+            forward_output().context("reporting failure")?;
             break output.status.code().unwrap_or(0);
         } else {
             eprintln!(" fixed {} assert_eq!s.", count);
